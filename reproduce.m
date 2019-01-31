9e9fde4605885b93f31fff3c790e261ba20aa799
Dimitra Danai Varsou <DemetraDanae@users.noreply.github.com>
2019-01-31 17:15:38 +0200

    Update reproduce.m

--- reproduce.m
+++ reproduce.m
@@ -7,3 +7,7 @@ newChrom (1,:) = [ Chromosomes(i1 ,1:point) Chromosomes(i2,point+1: nPoints) ];
 newChrom (2,:) = [ Chromosomes(i2 ,1:point) Chromosomes(i1,point+1: nPoints) ];
 
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Developed by Gerasimos Chourdakis - 2013, 2014 %%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
